ee988ef444cbbe39352096d4e40573d3a07db228
MadlyJado <madlyjado@proton.me>
2024-09-18 18:13:01 -0400

    Forgot to stage change!

--- app/hooks/useRegister.tsx
+++ app/hooks/useRegister.tsx
@@ -1,11 +1,13 @@
 import pb from "../lib/pocketbase"
 import Record from 'pocketbase'
 import { useMutation } from "react-query"
+import adminAuth from "../lib/adminauth";
 
 export default function useRegister() {
     async function register({ email, password }: any) {
+        
         const collection = pb.collection("users");
-
+        adminAuth();
         const record = collection.create({
             "email": email,
             "password": password,
