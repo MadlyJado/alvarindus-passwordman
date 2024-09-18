fb24b97f2aa4cf3cb4b9be512ec1a52b7f606b3c
MadlyJado <madlyjado@proton.me>
2024-09-18 19:20:10 -0400

    Hopefully this should work

--- app/hooks/useRegister.tsx
+++ app/hooks/useRegister.tsx
@@ -6,14 +6,18 @@ import adminAuth from "../lib/adminauth";
 export default function useRegister() {
     async function register({ email, password }: any) {
         
-        const collection = await pb.collection("users");
-        const record = await collection.create({
-            email: email,
-            password: password
-        })
+        const data = {
+            "email": email,
+            "emailVisibility": true,
+            "password": password,
+            "passwordConfirm": password,
+            "name": email
 
 
+        }; 
         adminAuth();
+
+        const record = await pb.collection("users").create(data);
     }
 
     return useMutation(register);
