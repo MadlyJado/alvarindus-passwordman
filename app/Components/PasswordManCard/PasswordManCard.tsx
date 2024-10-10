3998c9ef385f4ab2d6a175ab8b46fab987be82c0
MadlyJado <madlyjado@proton.me>
2024-10-10 17:06:33 -0400

    Hopfully improved some styling

--- app/Components/PasswordManCard/PasswordManCard.tsx
+++ app/Components/PasswordManCard/PasswordManCard.tsx
@@ -11,9 +11,9 @@ function PasswordManCard(props: {name: string, email: string, password: string})
     return (
         <>
             <div className="card">
-            <h1 className="bg-accent font-bold text-xl">User: {props.name}</h1>
-            <p className="bg-amber-500 text-base">Email: {props.email}</p>
-            <p className="bg-amber-300 text-base">Password: {props.password}</p>
+            <h1 className="card-title text-accent font-bold text-xl">{props.name}</h1>
+            <p className="text-amber-500 text-base">Email: {props.email}</p>
+            <p className="text-lime-700 text-base">Password: {props.password}</p>
             <button className="btn btn-accent btn-wide" onClick={CopyToClipBoard}>Click here to copy password of {props.email}</button>
             </div>
             
