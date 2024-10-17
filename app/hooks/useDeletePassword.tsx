542d547ca293ca611a1d77d5220a5b971f35c45f
MadlyJado <madlyjado@proton.me>
2024-10-17 15:59:47 -0400

    hi!

--- app/hooks/useDeletePassword.tsx
+++ app/hooks/useDeletePassword.tsx
@@ -1,11 +1,13 @@
-import { useState, useEffect } from 'react';
-import { useMutation } from 'react-query';
-import pb from '@/app/lib/pocketbase'; // Adjust the import path as necessary
-import { decryptPassword } from "@/app/lib/encryption"; // Adjust the import path as necessary
+import pb from "../lib/pocketbase";
+import { useMutation } from "react-query";
+import { decryptPassword } from "../lib/encryption";
+import crypto from 'crypto';
+import { useEffect, useState } from "react";
 
 export default function useDeletePassword() {
+    async function deletePassword(decryptedPassword: any) {
 
-    async function deletePassword({decryptedPassword}: any) {
+        
         const [data, setData] = useState<any[]>([]);
 
         useEffect(() => {
@@ -19,29 +21,29 @@ export default function useDeletePassword() {
                         console.error("Error fetching data:", error);
                     }
                 }
-            };
+             };
 
-            fetchData();
-        }, []);
+        fetchData();
+    }, []);
 
-        for (const item of data) {
-            const password = item.password;
-            const decryptedData = decryptPassword(password, localStorage.getItem(`iv_${item.id}`) || '', pb.authStore.model?.id || '');
-            if (decryptedData === decryptedPassword) {
-                await pb.collection("Account").delete(item.id);
-                localStorage.removeItem(`iv_${item.id}`);
-            }
+    data.forEach(async (item) => {
+        const password = item.password;
+        const decryptedData = decryptPassword(password, localStorage.getItem(`iv_${item.id}`) || '', pb.authStore.model?.id || '');
+        if (decryptedData == decryptedPassword) {
+            const isDeleted = await pb.collection("Account").delete(item.id);
+            localStorage.removeItem(`iv_${item.id}`);
         }
+    });
 
         
     }
 
     return useMutation(deletePassword, {
-        onSuccess: () => {
-            console.log('Password and IV deleted successfully');
+        onSuccess: (data) => {
+            console.log('Password and IV saved successfully');
         },
         onError: (error) => {
-            console.error('Failed to delete password:', error);
+            console.error('Failed to save password:', error);
         }
     });
 }
