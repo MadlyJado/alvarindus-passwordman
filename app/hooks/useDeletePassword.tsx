c5df5fe3a2ba802a2736cd1f403fad52f7aa3066
MadlyJado <madlyjado@proton.me>
2024-10-17 15:51:11 -0400

    ???

--- app/hooks/useDeletePassword.tsx
+++ app/hooks/useDeletePassword.tsx
@@ -3,26 +3,27 @@ import { useMutation } from 'react-query';
 import pb from '@/app/lib/pocketbase'; // Adjust the import path as necessary
 import { decryptPassword } from "@/app/lib/encryption"; // Adjust the import path as necessary
 
-export default function useDeletePassword(decryptedPassword: string) {
-    const [data, setData] = useState<any[]>([]);
+export default function useDeletePassword() {
 
-    useEffect(() => {
-        const fetchData = async () => {
-            if (pb.authStore.model) {
-                const usremail = pb.authStore.model.email;
-                try {
-                    const records = await pb.collection("Account").getFullList({ filter: `user="${usremail}"` });
-                    setData(records);
-                } catch (error) {
-                    console.error("Error fetching data:", error);
+    async function deletePassword(decryptedPassword: string) {
+        const [data, setData] = useState<any[]>([]);
+
+        useEffect(() => {
+            const fetchData = async () => {
+                if (pb.authStore.model) {
+                    const usremail = pb.authStore.model.email;
+                    try {
+                        const records = await pb.collection("Account").getFullList({ filter: `user="${usremail}"` });
+                        setData(records);
+                    } catch (error) {
+                        console.error("Error fetching data:", error);
+                    }
                 }
-            }
-        };
+            };
 
-        fetchData();
-    }, []);
+            fetchData();
+        }, []);
 
-    const deletePassword = async () => {
         for (const item of data) {
             const password = item.password;
             const decryptedData = decryptPassword(password, localStorage.getItem(`iv_${item.id}`) || '', pb.authStore.model?.id || '');
@@ -31,9 +32,8 @@ export default function useDeletePassword(decryptedPassword: string) {
                 localStorage.removeItem(`iv_${item.id}`);
             }
         }
-    };
 
-    const mutation = useMutation(deletePassword, {
+    const mutation: any = useMutation(deletePassword, {
         onSuccess: () => {
             console.log('Password and IV deleted successfully');
         },
@@ -41,12 +41,6 @@ export default function useDeletePassword(decryptedPassword: string) {
             console.error('Failed to delete password:', error);
         }
     });
-
-    useEffect(() => {
-        if (data.length > 0) {
-            mutation.mutate();
-        }
-    }, [data, decryptedPassword]);
 
     return mutation;
 }
