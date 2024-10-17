af66bc2bc3bd981f7675df792032452e1b50ce35
MadlyJado <madlyjado@proton.me>
2024-10-17 13:27:10 -0400

    Figured out I had forgotten to put in the attribute of deletePassword function, so I fixed that

--- app/Components/PasswordManCard/PasswordManCard.tsx
+++ app/Components/PasswordManCard/PasswordManCard.tsx
@@ -7,6 +7,12 @@ function PasswordManCard(props: {name: string, email: string, password: string})
         navigator.clipboard.writeText(props.password);
     }
 
+    function deletePass() {
+        deletePassword(props.password);
+        // Reload the page to reflect the change
+        window.location.reload();  // This line is not required in a functional component, but is required in a class component
+    }
+
     const { mutate: deletePassword} =  useDeletePassword();
 
     return (
@@ -16,7 +22,7 @@ function PasswordManCard(props: {name: string, email: string, password: string})
             <p className="text-amber-500 text-base">Email: {props.email}</p>
             <p className="text-lime-700 text-base">Password: {props.password}</p>
             <button className="btn btn-accent btn-wide" onClick={CopyToClipBoard}>Click here to copy password of {props.email}</button>
-            <button className="btn btn-accent btn-wide" onClick={deletePassword}>Click here to delete password entry</button>
+            <button className="btn btn-accent btn-wide" onClick={deletePass}>Click here to delete password entry</button>
             </div>
             
         
