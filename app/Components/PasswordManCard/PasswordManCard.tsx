e8708f1d00eaf7ad97bbdb9e106362f504673fad
MadlyJado <madlyjado@proton.me>
2024-10-17 16:01:47 -0400

    stuffs stuff

--- app/Components/PasswordManCard/PasswordManCard.tsx
+++ app/Components/PasswordManCard/PasswordManCard.tsx
@@ -1,39 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useDeletePassword from "@/app/hooks/useDeletePassword";
 import { encryptPassword } from "@/app/lib/encryption";
 import pb from "@/app/lib/pocketbase";
 
-function PasswordManCard(props: {name: string, email: string, password: string}) {
-
-
+function PasswordManCard(props: { name: string, email: string, password: string }) {
+    const { mutate: deletePass, isSuccess } = useDeletePassword();
 
     function CopyToClipBoard() {
         navigator.clipboard.writeText(props.password);
     }
 
-
     function deletePassword() {
-       const { mutate: deletePass } = useDeletePassword();
-       deletePass({decryptedPassword: props.password});
+        deletePass({ decryptedPassword: props.password });
     }
+
+    useEffect(() => {
+        if (isSuccess) {
+            window.location.reload();
+        }
+    }, [isSuccess]);
 
     return (
         <>
             <div className="card">
-            <h1 className="card-title text-accent font-bold text-xl">{props.name}</h1>
-            <p className="text-amber-500 text-base">Email: {props.email}</p>
-            <p className="text-lime-700 text-base">Password: {props.password}</p>
-            <button className="btn btn-accent btn-wide" onClick={CopyToClipBoard}>Click here to copy password of {props.email}</button>
-            <button className="btn btn-accent btn-wide" onClick={deletePassword}>Click here to delete password entry</button>
+                <h1 className="card-title text-accent font-bold text-xl">{props.name}</h1>
+                <p className="text-amber-500 text-base">Email: {props.email}</p>
+                <p className="text-lime-700 text-base">Password: {props.password}</p>
+                <button className="btn btn-accent btn-wide" onClick={CopyToClipBoard}>Click here to copy password of {props.email}</button>
+                <button className="btn btn-accent btn-wide" onClick={deletePassword}>Click here to delete password entry</button>
             </div>
-            
-        
         </>
-    )
+    );
 }
 
-PasswordManCard.propTypes = {
+PasswordManCard.propTypes = {};
 
-}
-
-export default PasswordManCard
+export default PasswordManCard;
