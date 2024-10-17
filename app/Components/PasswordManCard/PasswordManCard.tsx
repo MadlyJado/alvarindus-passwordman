5cd34120b9d8a4bc99050a766ed7739699abb5ac
MadlyJado <madlyjado@proton.me>
2024-10-17 16:33:12 -0400

    maybe??

--- app/Components/PasswordManCard/PasswordManCard.tsx
+++ app/Components/PasswordManCard/PasswordManCard.tsx
@@ -1,17 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useDeletePassword from "@/app/hooks/useDeletePassword";
 import { encryptPassword } from "@/app/lib/encryption";
 import pb from "@/app/lib/pocketbase";
 
-function PasswordManCard(props: { name: string, email: string, password: string }) {
+function PasswordManCard(props: { name: string, email: string, password: string, encryptedPassword: string }) {
+
+    const [data, setData] = useState<any[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (pb.authStore.model) {
+                const usremail = pb.authStore.model.email;
+                try {
+                    const records = await pb.collection("Account").getFullList({ filter: `user="${usremail}"` });
+                    setData(records);
+                } catch (error) {
+                    console.error("Error fetching data:", error);
+                }
+            }
+        };
+
+        fetchData();
+    }, []);
+
     const { mutate: deletePass, isSuccess } = useDeletePassword();
 
     function CopyToClipBoard() {
         navigator.clipboard.writeText(props.password);
     }
 
-    function deletePassword() {
-        deletePass(props.password);
+    async function deletePassword() {
+       data.map(async (item) => {
+            if(item.password === props.encryptedPassword) {
+                await deletePass({ id: item.id, encryptedPassword: props.encryptedPassword });
+                return;
+            }
+       }
     }
 
     useEffect(() => {
