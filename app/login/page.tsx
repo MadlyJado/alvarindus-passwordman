3779be92fa29b107c59cea4d6011979bd852fa32
MadlyJado <madlyjado@proton.me>
2024-11-11 22:06:26 -0500

    Fixed styling of login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,7 +9,7 @@ const Login: NextPage<Props> = ({}) => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-orange-500 to-red-700 text-white" >
         <h1 className="text-secondary text-center text-xl bg-transparent">Login Page</h1>
         <EmailAndPass/>
-        <Link href="/" className="btn btn-secondary">Back to homepage</Link>
+        <Link href="/" className="btn btn-secondary btn-lg btn-wide shadow-lg hover:scale-105 transform transition-all duration-300">Back to homepage</Link>
     </div>
   )
 }
