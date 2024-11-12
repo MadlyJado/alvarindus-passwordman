181672ff0ee38ad39a67722404ea766baf93de4b
MadlyJado <madlyjado@proton.me>
2024-11-11 23:20:31 -0500

    adsadsdsa

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 interface Props {}
 
-const page: NextPage<Props> = ({}) => {
+const Page: NextPage<Props> = ({}) => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-yellow-400 to-yellow-700 text-white">
         <h1 className="text-4xl font-extrabold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-teal-200 to-lime-200">
@@ -21,4 +21,4 @@ const page: NextPage<Props> = ({}) => {
   )
 }
 
-export default page
+export default Page
