f8953edbb882dacf3c892ab4eb2c0f15b5e69f3d
MadlyJado <madlyjado@proton.me>
2024-11-11 23:33:57 -0500

    Fixed styling for mobile use

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,14 +5,17 @@ import pb from '../lib/pocketbase';
 
 const Login: NextPage = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-orange-500 to-red-700 text-white">
-      <EmailAndPass />
-      <Link href="/">
-        <p className="mt-6 btn btn-secondary btn-lg btn-wide shadow-lg hover:scale-105 transform transition-all duration-300">
-          Back to Homepage
-        </p>
-      </Link>
-    </div>
+    <body className="bg-gradient-to-b from-orange-500 to-red-700 text-white">
+        <div className="min-h-screen flex flex-col items-center justify-center ">
+        <EmailAndPass />
+        <Link href="/">
+          <p className="mt-6 btn btn-secondary btn-lg btn-wide shadow-lg hover:scale-105 transform transition-all duration-300">
+            Back to Homepage
+          </p>
+        </Link>
+      </div>
+    </body>
+    
   )
 }
 
