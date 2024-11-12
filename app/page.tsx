737ea7c323be6145cea912321411f62d2c722714
MadlyJado <madlyjado@proton.me>
2024-11-11 21:08:39 -0500

    New styling!!11

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-500 to-indigo-700 text-white">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-950 to-indigo-700 text-white">
       <h1 className="text-4xl font-extrabold mb-8 text-white bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-lime-200">
         Alvarindus Password Manager
       </h1>
