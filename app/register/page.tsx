4f6a4e205b53cb77bb354ca9fc5da559b9c1592b
MadlyJado <madlyjado@proton.me>
2024-11-11 23:30:27 -0500

    sdsadsds

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -7,17 +7,20 @@ interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-yellow-400 to-yellow-700 text-white">
-        <h1 className="text-4xl font-extrabold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-teal-200 to-lime-200">
-          Register
-        </h1>
-        <Register/>
-        <Link href="/">
-        <p className="mt-6 btn btn-secondary btn-lg shadow-lg hover:scale-105 transform transition-all duration-300 w-full">
-          Back to Homepage
-        </p>
-      </Link>
-    </div>
+    <body className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-yellow-400 to-yellow-700 text-white">
+      <div >
+          <h1 className="text-4xl font-extrabold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-teal-200 to-lime-200">
+            Register
+          </h1>
+          <Register/>
+          <Link href="/">
+          <p className="mt-6 btn btn-secondary btn-lg shadow-lg hover:scale-105 transform transition-all duration-300 w-full">
+            Back to Homepage
+          </p>
+        </Link>
+      </div>
+    </body>
+    
   )
 }
 
