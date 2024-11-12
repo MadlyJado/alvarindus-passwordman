403080a7b3802bf7a41bfb625e4d555e24f4c029
MadlyJado <madlyjado@proton.me>
2024-11-12 02:21:12 -0500

    Did some styling for CreateNewPassword page

--- app/Components/CreateNewPassword/CreateNewPassword.tsx
+++ app/Components/CreateNewPassword/CreateNewPassword.tsx
@@ -16,21 +16,23 @@ function CreateNewPassword() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-    <label className="input input-bordered flex items-center gap-2">
-        <input type="text" className="grow" placeholder="Name" {...register("name")}/>
-    </label>
-    <label className="input input-bordered flex items-center gap-2">
-        <input type="text" className="grow" placeholder="Email" {...register("email")}/>
-    </label>
-    <label className="input input-bordered flex items-center gap-2">
-        <input type="password" className="grow" placeholder="Password" {...register("password")}/>
-    </label>
-    <label className="input input-bordered flex items-center gap-2">
-        <input className="grow" placeholder="URL" {...register("url")}/>
-    </label>
-    <button className="btn btn-primary" type="submit" disabled={isLoading}>{isLoading ? "Loading": "Create New Login Info Entry"}</button>
-  </form>
+    <div className="text-4xl font-extrabold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-teal-200 to-lime-200">
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <label className="input input-bordered w-full">
+            <input type="text" className="grow" placeholder="Name" {...register("name")}/>
+        </label>
+        <label className="input input-bordered w-full">
+            <input type="text" className="grow" placeholder="Email" {...register("email")}/>
+        </label>
+        <label className="input input-bordered w-full">
+            <input type="password" className="grow" placeholder="Password" {...register("password")}/>
+        </label>
+        <label className="input input-bordered w-full">
+            <input className="grow" placeholder="URL" {...register("url")}/>
+        </label>
+        <button className="btn btn-primary btn-lg shadow-lg hover:scale-105 transform transition-all duration-300 w-full" type="submit" disabled={isLoading}>{isLoading ? "Loading": "Create New Login Info Entry"}</button>
+    </form>
+  </div>
   )
 }
 
--- app/createnewpassword/page.tsx
+++ app/createnewpassword/page.tsx
@@ -7,10 +7,14 @@ interface Props {}
 
 const createnewpassword: NextPage<Props> = ({}) => {
   return (
-    <div >
+    <div className="bg-gradient-to-b from-emerald-400 to-teal-800 text-white">
         <h1 className="text-secondary text-center text-xl bg-transparent">Create New Password</h1>
         <CreateNewPassword/>
-        <Link href="/login" className="btn btn-secondary">Back to logged in page</Link>
+        <Link href="/login" >
+          <p className="mt-6 btn bg-amber-700 btn-lg btn-wide shadow-lg hover:scale-105 transform transition-all duration-300">
+            Back to logged in page
+          </p>
+        </Link>
     </div>
   )
 }
