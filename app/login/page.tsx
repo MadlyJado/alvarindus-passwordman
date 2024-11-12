9f0764fa51c789e1936bc3241a72a6d69bce30d8
MadlyJado <madlyjado@proton.me>
2024-11-11 22:18:08 -0500

    dasdasdasd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,9 @@ const Login: NextPage<Props> = ({}) => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-orange-500 to-red-700 text-white" >
       <div className="flex gap-8">
-      <h1 className="text-secondary text-center text-xl bg-transparent">Login Page</h1>
+      <h1 className="text-4xl font-extrabold mb-8 text-white bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-lime-200">
+         Login page
+      </h1>
         <EmailAndPass/>
         <Link href="/">
           <p className="btn btn-secondary btn-lg btn-wide shadow-lg hover:scale-105 transform transition-all duration-300">
