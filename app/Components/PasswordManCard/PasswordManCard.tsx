3e44e28833344c643d8a8d59d02141e81cf682fb
MadlyJado <madlyjado@proton.me>
2024-11-12 02:38:05 -0500

    maybe??

--- app/Components/PasswordManCard/PasswordManCard.tsx
+++ app/Components/PasswordManCard/PasswordManCard.tsx
@@ -23,7 +23,7 @@ function PasswordManCard(props: { name: string, email: string, password: string,
 
     return (
         <>
-            <div className="card flex flex-col gap-12 bg-white">
+            <div className="card flex flex-col gap-x-12 bg-white">
                 <h1 className="card-title text-accent font-bold text-xl">{props.name}</h1>
                 <p className="text-amber-500 text-base">URL: {props.url}</p>
                 <p className="text-amber-500 text-base">Email: {props.email}</p>
