504b40f1c8c4f5b4ca90939f76d7e33a2b55e8f1
MadlyJado <madlyjado@proton.me>
2024-11-11 23:23:01 -0500

    did stuff

--- app/Components/Register/Register.tsx
+++ app/Components/Register/Register.tsx
@@ -16,22 +16,19 @@ function Register(props: any) {
 
 
     return (
-            
-        <>
-            <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg items-center">
-                {isLoading && <p className="text-center text-lg">Loading...</p>}
-                {isError && <p className="text-center text-lg bg-red-400 p-2 rounded">Invalid email or password</p>}
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <label className="input input-bordered w-full">
-                        <input type="text" className="grow" placeholder="Email" {...register("email")}/>
-                    </label>
-                    <label className="input input-bordered w-full">
-                        <input type="password" className="grow" placeholder="Password" {...register("password")}/>
-                    </label>
-                    <button className="btn btn-primary btn-lg shadow-lg hover:scale-105 transform transition-all duration-300 w-full" type="submit" disabled={isLoading}>{isLoading ? "Registering": "Register"}</button>
-                </form>
-            </div>
-        </>
+        <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg items-center">
+            {isLoading && <p className="text-center text-lg">Loading...</p>}
+            {isError && <p className="text-center text-lg bg-red-400 p-2 rounded">Invalid email or password</p>}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label className="input input-bordered w-full">
+                    <input type="text" className="grow" placeholder="Email" {...register("email")}/>
+                </label>
+                <label className="input input-bordered w-full">
+                    <input type="password" className="grow" placeholder="Password" {...register("password")}/>
+                </label>
+                <button className="btn btn-primary btn-lg shadow-lg hover:scale-105 transform transition-all duration-300 w-full" type="submit" disabled={isLoading}>{isLoading ? "Registering": "Register"}</button>
+            </form>
+        </div>
     )
 }
 
