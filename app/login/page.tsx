cc347e3dcd0f7d417754b0f8cb37c90a19ee4224
MadlyJado <madlyjado@proton.me>
2024-11-11 22:12:35 -0500

    Created new styling fixes for login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,6 +7,15 @@ interface Props {}
 const Login: NextPage<Props> = ({}) => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-orange-500 to-red-700 text-white" >
+      <div className="flex gap-8">
+      <h1 className="text-secondary text-center text-xl bg-transparent">Login Page</h1>
+        <EmailAndPass/>
+        <Link href="/" >
+          <p className="btn btn-secondary btn-lg btn-wide shadow-lg hover:scale-105 transform transition-all duration-300">
+            Back to homepage
+          </p>
+        </Link>
+      </div>
         <h1 className="text-secondary text-center text-xl bg-transparent">Login Page</h1>
         <EmailAndPass/>
         <Link href="/" className="btn btn-secondary btn-lg btn-wide shadow-lg hover:scale-105 transform transition-all duration-300">Back to homepage</Link>
