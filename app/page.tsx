ef55208f0096fc31551bad13d5694086668b8bf4
MadlyJado <madlyjado@proton.me>
2024-11-14 17:58:48 -0500

    adsadsad

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-950 to-indigo-700 text-white p-4">
+    <div className="bg-[url('/lib/Jadons Pattern.png')]  min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-950 to-indigo-700 text-white p-4">
       <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold mb-8 text-white bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-lime-200 text-center">
         Alvarindus Password Manager
       </h1>
