70ac06bdc6c41b68ffce38034039ee1b14836f09
MadlyJado <madlyjado@proton.me>
2024-11-21 17:48:44 -0500

    I hope this works!!

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-950 to-indigo-700 text-white p-4">
       <div className="grid grid-cols-6 absolute size-9 opacity-30">
-        <img href="./lib/pattern.png" alt="Pattern"></img>
-        <img href="./lib/pattern.png" alt="Pattern"></img>
-        <img href="./lib/pattern.png" alt="Pattern"></img>
-        <img href="./lib/pattern.png" alt="Pattern"></img>
-        <img href="./lib/pattern.png" alt="Pattern"></img>
-        <img href="./lib/pattern.png" alt="Pattern"></img>
-        <img href="./lib/pattern.png" alt="Pattern"></img>
-        <img href="./lib/pattern.png" alt="Pattern"></img>
-        <img href="./lib/pattern.png" alt="Pattern"></img>
-        <img href="./lib/pattern.png" alt="Pattern"></img>
+        <Image src="@/app/lib/pattern.png" alt="Pattern"></Image>
+        <Image src="@/app/lib/pattern.png" alt="Pattern"></Image>    
+        <Image src="@/app/lib/pattern.png" alt="Pattern"></Image>    
+        <Image src="@/app/lib/pattern.png" alt="Pattern"></Image>    
+        <Image src="@/app/lib/pattern.png" alt="Pattern"></Image>    
+        <Image src="@/app/lib/pattern.png" alt="Pattern"></Image>    
+        <Image src="@/app/lib/pattern.png" alt="Pattern"></Image>    
+        <Image src="@/app/lib/pattern.png" alt="Pattern"></Image>          
       </div>
       
       <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold mb-8 text-white bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-lime-200 text-center">
